03ac7310572a9ec0a14d803ecf864ed3618e964e
sneha....... <snehakumari76373@gmail.com>
2025-09-12 12:32:37 +0530

    Update app.js

--- app.js
+++ app.js
@@ -38,7 +38,7 @@ async function main() {
 }
 
  app.set("view engine","ejs");
- app.set("views",path.join(__dirname,"views"));
+ app.set("views",path.join(__dirname,"Views"));
  app.use(express.urlencoded({ extended:true}));
  app.use(methodOverride("_method"));
  app.engine('ejs', ejsMate);
